b73d5bfc70be9f73533b91e6a19f348709aa8eb0
Marcel Haidacher <marcel.haidacher@etalytics.com>
2025-02-11 15:40:18 +0100

    added report

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -23,6 +23,9 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+  reporter: [
+    ['playwright-ctrf-json-reporter', {}]
+  ],
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm exec nx run etalytics:serve',
